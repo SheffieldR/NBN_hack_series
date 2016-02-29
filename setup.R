a2ecfc3871e03332d1fb80cb73e1ddd3f2c398c7
Mathew Hall <mathew.james.hall@gmail.com>
2016-02-29 17:23:42 +0000

    Add code to setup environment (install rnbn & login to NBN)

---
+++ setup.R
@@ -0,0 +1,34 @@
+
+if(!library('rnbn', logical.return=TRUE)){ 
+  install.packages("rnbn") 
+}
+library(rnbn)
+
+if(file.exists('NBN_Details.R')){
+  source("NBN_Details.R")
+}else{
+  writeLines("NBN_Details.R doesn't exist. Do you want to create it?")
+  create <- readline("Create file (y/n)? ")
+  retry <- tolower(create) == "y"
+  while(retry){
+    user <- readline("NBN username: ")
+    password <- readline("NBN password: ")
+    retry <- tryCatch(
+      {
+        rnbn::nbnLogin(user,password);
+        sink("NBN_Details.R");
+        writeLines(paste0("NBN_USER <- '", gsub("'", "\\'", user), "';"));
+        writeLines(paste0("NBN_PASSWORD <- '", gsub("'", "\\'", password), "';"));
+        sink();
+        source("NBN_Details.R")
+        FALSE
+      },
+      error = function(e){
+        browser()
+        choice<- readline("Login failed. Try again (y/n)? ")
+        tolower(choice) == 'y'
+      }
+    )
+    
+  }
+}
