ff4be37958e7e149dd03edfbf1d5852cfe339374
Tom August <tomaug@ceh.ac.uk>
2016-03-02 12:09:29 +0000

    improve commenting

--- Team_Temporal/get_NBN_data.R
+++ Team_Temporal/get_NBN_data.R
@@ -2,45 +2,50 @@
 # data for the hectad listed in the csv.
 
 # This csv has the hectads of south west yorkshire
+# I got it from the biological records centre's website
 
 rm(list = ls())
 library(rnbn)
 
 # Which hectads are around Sheffield 
-shef_hec_tab <- read.csv('south-west-yorkshire-10km-squares.csv', stringsAsFactors = FALSE)
+shef_hec_tab <- read.csv('Team_Temporal/south-west-yorkshire-10km-squares.csv',
+                         stringsAsFactors = FALSE)
 shef_hec <- shef_hec_tab$Square
 shef_hec
 
-# Function for doing a few hectads
-# Beefed up a bit
+# Function for doing data retrieval
 multi_hectad <- function(x){
   
   # read in
   # for this to work you will need to create this
   # r data file with your login details
   # or comment out this line and type in your username
-  # and password in teh nbnLogin function
-  load(file = 'login_details.rdata')
+  # and password in the nbnLogin function below
+  load(file = 'Team_Temporal/login_details.rdata')
   
   # login
   nbnLogin(username = login_details$un,
            password = login_details$pwd)
+  
   # get data
   occ <- getOccurrences(gridRef = x)
   
   # write out a csv copy
   # (we will also create a .rdata copy for faster loading)
-  write.table(x = occ, file = 'shef_table.csv', sep = ',', append = TRUE, col.names = TRUE)
+  write.table(x = occ, file = 'Team_Temporal/shef_table.csv',
+              sep = ',', append = TRUE, col.names = TRUE)
   
   # write out data about data contributors
-  write.table(x = attr(occ, which = 'providers'), file = 'shef_providers.csv', sep = ',', append = TRUE, col.names = TRUE)
+  write.table(x = attr(occ, which = 'providers'),
+              file = 'Team_Temporal/shef_providers.csv',
+              sep = ',', append = TRUE, col.names = TRUE)
   
   return(occ)
   
 }
 
-
 ### Run in parallel
+# this speeds things up if you have multiple cores
 # Load libraries for parallelising
 library(parallel)
 library(snowfall)
@@ -57,7 +62,9 @@ sfLibrary(rnbn)
 
 # Running in parallel is silent - nothing will appear in your console
 time_taken <- system.time({
+  
   shef_data <- do.call(rbind, sfClusterApplyLB(shef_hec, multi_hectad))
+  
 })
 
 print(time_taken)
