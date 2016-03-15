2430396f4a0e3a6e735938eae29a60f8dae2c9ce
Mathew Hall <mathew.james.hall@gmail.com>
2016-03-15 17:14:07 +0000

    Wrap Shef population in a function

--- Humans_Vs_Biodiversity/population.R
+++ Humans_Vs_Biodiversity/population.R
@@ -2,12 +2,15 @@ library(dplyr)
 
 library(reshape2)
 library(tidyr)
-population.data <- read.csv("MYEB3_summary_components_of_change_series_UK_(0214).csv", stringsAsFactors=F) %>%
-  melt(id.vars = 1:3) %>%
-  extract(variable, into=c("measurement","year"), regex="(.*)_(\\d{4})")
-#  mutate(year = )
 
-sheffield.population <- population.data %>% 
-  filter(lad2014_name == "Sheffield") %>%
-  filter(measurement  == "population") %>%
-  select(year, population=value)
+sheffield.population <- function(){
+  population.data <- read.csv("MYEB3_summary_components_of_change_series_UK_(0214).csv", stringsAsFactors=F) %>%
+    melt(id.vars = 1:3) %>%
+    extract(variable, into=c("measurement","year"), regex="(.*)_(\\d{4})")
+  #  mutate(year = )
+
+  population.data %>% 
+    filter(lad2014_name == "Sheffield") %>%
+    filter(measurement  == "population") %>%
+    select(year, population=value)
+}
