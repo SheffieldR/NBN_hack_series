839d035c9141488c682911cf8dda75236e2cb030
Mathew Hall <mathew.james.hall@gmail.com>
2016-03-15 17:11:24 +0000

    Add ONS population data

---
+++ Humans_Vs_Biodiversity/README.md
@@ -0,0 +1,3 @@
+# Data Attribution
+
+Includes data taken from the ONS [population estimates](http://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/bulletins/annualmidyearpopulationestimates/2015-06-25).
---
+++ Humans_Vs_Biodiversity/population.R
@@ -0,0 +1,9 @@
+library(dplyr)
+
+library(reshape2)
+library(tidyr)
+population.data <- read.csv("MYEB3_summary_components_of_change_series_UK_(0214).csv", stringsAsFactors=F) %>%
+  melt(id.vars = 1:3) %>%
+  extract(variable, into=c("measurement","year"), regex="(.*)_(\\d{4})")
+#  mutate(year = )
+  
