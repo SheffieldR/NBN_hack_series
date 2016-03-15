9a2e2940377cfd268d0dc9239fc2111dcc55815e
Mathew Hall <mathew.james.hall@gmail.com>
2016-03-15 17:35:23 +0000

    Add analysis of population vs distinct species count

--- Humans_Vs_Biodiversity/Analysis.R
+++ Humans_Vs_Biodiversity/Analysis.R
@@ -3,19 +3,21 @@ library("dplyr")
 library("ggplot2")
 library("ggmap")
 library("tidyr")
+source("population.R")
 
+pop.history <- sheffield.population()
 
-dipshef <- getOccurrences(tvks = 'NBNSYS0000166512', gridRef = 'Sk38', silent = TRUE, acceptTandC = TRUE)
+dipshef <- getOccurrences(gridRef = 'SK38', silent = TRUE, acceptTandC = TRUE, startYear = 2000)
 dipshef
 
 #this seperates the startDate column into seperate columns for years, months and days so that we 
 # can just make a year on year map
-dipshef2 <- separate(dipshef, startDate, c("y", "m", "d"))
+dipshef2 <- separate(dipshef, startDate, c("year", "m", "d"))
 dipshef2
 
 # change y column to num from char
-dipshef3 <- mutate(dipshef2, y = as.numeric(y)) %>%
-  mutate(decade = floor(y/10)*10)
+dipshef3 <- mutate(dipshef2, year = as.numeric(year)) %>%
+  mutate(decade = floor(year/10)*10)
 
 #get a google map for sheffield
 
@@ -29,3 +31,10 @@ mapPoints <- ggmap(map) +
   geom_point(aes(x = longitude, y = latitude, color = factor(decade)), size = 5, alpha = .3, data = dipshef3)
 
 mapPoints
+
+
+dipshef2 %>% 
+  filter(year >= min(pop.history$year)) %>% 
+  group_by(year) %>% 
+  summarise(count=length(unique(pTaxonVersionKey))) %>%
+  left_join(pop.history) %>% ggplot(aes(population, count)) + geom_point() + geom_smooth(method="lm") + geom_text(aes(label=year))
